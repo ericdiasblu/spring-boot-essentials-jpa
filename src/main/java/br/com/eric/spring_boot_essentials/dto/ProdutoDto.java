0f3d128689c3b0c7d58c848a347ff7217e66b51c
Eric Dias <ericdiasblu@gmail.com>
2026-04-13 15:11:10 -0300

    feat: aula01

---
+++ src/main/java/br/com/eric/spring_boot_essentials/dto/ProdutoDto.java
@@ -0,0 +1,19 @@
+package br.com.eric.spring_boot_essentials.dto;
+
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ProdutoDto {
+
+    private String nome;
+    private BigDecimal preco;
+    private Integer quantidade;
+}
